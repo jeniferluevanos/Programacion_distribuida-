c8a468bbb828ca6b0f09631d435549b00bf70c95
Jeni <jluevanos@ucol.mx>
2022-03-09 09:11:07 -0600

    Views con pug y plantilla

---
+++ P4/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const md5 = require('md5')
+const { appConfig } = require('./lib/config')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/plantilla', function(req, res) {
+    res.render('plantilla');
+});
+
+app.get('/practica', function(req, res) {
+    res.render('practica');
+});
+
+app.post('/ingreso', function(req, res) {
+    //res.send(`Has ingresado con el correo ${req.body.email} y con la contraseña ${req.body.pass}`)
+    let pass = md5(req.body.pass)
+    res.render('ingreso', {correo: req.body.email, pass: pass});
+});
+
+app.listen(appConfig.port, ()=> console.log(`Puesto en marcha en puerto ${appConfig.port}`)) 
